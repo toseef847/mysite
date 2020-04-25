b41b60becc0f9981b489c3b5af893a407a912f69
toseef847 <47262334+toseef847@users.noreply.github.com>
2020-04-26 01:15:00 +0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mysite',
         'USER': 'postgres',
-        'PASSWORD': 'Toseef847',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
